b5ecd8c0bfc7cdede0495e13e6ba41941df5fd96
jonathan.silva <jonathan.silva@corp.globo.com>
2018-06-03 17:19:04 -0300

    Add the Broyden method and use numpy array properties on operations

--- RootFinding/broyden.py
+++ RootFinding/broyden.py
@@ -15,32 +15,36 @@
 import numpy as np
 
 tolerance = 0.00000001
-iterations_limit = 10000
+iterations_limit = 7
 
-def multi_newton(x0_vector, f_vector, jacobian_vector):
+def broyden(x0_vector, initial_approx_Jacobian, f_vector):
 
     x_vector = x0_vector
+    approx_Jacobian = initial_approx_Jacobian
     for iteration in range(iterations_limit):
 
-        jacobian_current_values = jacobian_vector(x_vector)
+        jacobian_current_values = approx_Jacobian
         function_current_values = f_vector(x_vector)
         
-        delta_x = -1*(np.linalg.inv(jacobian_current_values)
-                      @ function_current_values)
+        delta_x = -1*(np.matmul(np.linalg.inv(jacobian_current_values),
+                                function_current_values))
 
         x_vector = x_vector + delta_x
+        delta_f = f_vector(x_vector) - function_current_values
 
         if(np.linalg.norm(delta_x)/np.linalg.norm(x_vector) \
             < tolerance):
             return x_vector
+        else:
+            jacob_correction = ((delta_f - approx_Jacobian 
+                                        @ delta_x) @ np.transpose(delta_x))\
+                               /(np.transpose(delta_x) @ delta_x)
+            approx_Jacobian = approx_Jacobian + jacob_correction
 
-    return "Convergence not reached"
+    return "Convergence not reached: \n" + str(x_vector)
 
 def f_vector(x_vector):
     return np.array([[x_vector[0][0] + 2*x_vector[1][0] - 2.0], 
             [pow(x_vector[0][0], 2) + 4*pow(x_vector[1][0], 2) - 4]])
 
-def jacobian_vector(x_vector):
-    return np.array([[1, 2], [2*x_vector[0][0], 8*x_vector[1][0]]])
-
-print(multi_newton(np.array([[2], [3]]), f_vector, jacobian_vector))
+print(broyden(np.array([[2], [3]]), np.array([[1, 2], [4, 24]]), f_vector))
